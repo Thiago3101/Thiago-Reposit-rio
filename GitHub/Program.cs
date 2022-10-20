5edd8699021a84a81d652d7e2bf6ff62059ec4f4
Thiago3101 <116303614+Thiago3101@users.noreply.github.com>
2022-10-20 14:51:00 -0400

    Programei a opção 1
    Adicionei a opção de saudação

--- GitHub/Program.cs
+++ GitHub/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("olá, Mundo!");
+﻿Console.WriteLine("--------------------------");
+Console.WriteLine("[1] - Saudação.");
+Console.WriteLine("--------------------------");
+
+Console.Write("\nEscolha uma opção: ");
+int opc = Convert.ToInt32(Console.ReadLine());
+
+if(opc == 1)
+{
+    Console.WriteLine("\nQual seu nome?");
+    string nome = Console.ReadLine();
+    Console.WriteLine($"\nOlá, {nome}!");
+}
